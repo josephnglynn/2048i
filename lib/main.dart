84fc6199ab17ec5cb1cd4a6ca96302f7d5dfed65
josephnglynn <josephnglynn@hotmail.co.uk>
2021-05-07 08:27:00 +0100

    fix notification bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:improved_2048/ui/homePage.dart';
-import 'package:flutter/services.dart';
 import 'api/settings.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Settings.init();
-  SystemChrome.setEnabledSystemUIOverlays([]);
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark(),

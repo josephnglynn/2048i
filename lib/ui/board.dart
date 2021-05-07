ee89e787e6a9944a8ba8fcef69aecdc4d6e2a957
josephnglynn <josephnglynn@hotmail.co.uk>
2021-05-07 08:01:45 +0100

    hide top bar

--- lib/ui/board.dart
+++ lib/ui/board.dart
@@ -1,12 +1,10 @@
 import 'dart:math';
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:improved_2048/api/settings.dart';
 import 'package:improved_2048/ui/types.dart';
-
 import 'highScore.dart';
 
 class BoardPainter extends CustomPainter {

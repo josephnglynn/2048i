d7c87786a6a75dda89f5771f6c86345b7bf2f1ef
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-05-11 21:32:33 +0100

    Added Login Page

--- lib/ui/authenticationPage.dart
+++ lib/ui/authenticationPage.dart
@@ -2,12 +2,15 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:improved_2048/api/auth.dart';
+import 'package:improved_2048/api/leaderBoard.dart';
+import 'package:improved_2048/ui/homePage.dart';
+import 'package:improved_2048/ui/leaderBoardPage.dart';
 
 class AuthenticationDialog extends StatefulWidget {
   static Future showAuthDialog(BuildContext context) async {
-    var dialog = Dialog(
-      child: AuthenticationDialog(),
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor.withOpacity(0),
+    final dialog = AlertDialog(
+      content: AuthenticationDialog(),
     );
     await showDialog(
       context: context,
@@ -23,55 +26,164 @@ class _AuthenticationDialogState extends State<AuthenticationDialog> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          Theme.of(context).scaffoldBackgroundColor.withOpacity(0.75),
       body: SafeArea(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-          child: Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: 10,
-                      right: 10,
-                      bottom: MediaQuery.of(context).size.height / 3),
-                  child: Text(
-                    "Oop, you have to be logged in to access online services",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          color: Theme.of(context).textTheme.bodyText1!.color,
-                          fontSize: 25),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                          color: Theme.of(context).textTheme.bodyText1!.color,
-                          fontSize: 25),
-                    ),
-                  ),
-                ),
-              ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 10,
+                  right: 10,
+                  bottom: MediaQuery.of(context).size.height / 3),
+              child: Text(
+                "Oop, you have to be logged in to access online services!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyText1!.color,
+                      fontSize: 25),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyText1!.color,
+                      fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController name = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme:
+            IconThemeData(color: Theme.of(context).textTheme.bodyText1!.color),
+        title: Text(
+          "Login",
+          style: TextStyle(color: Theme.of(context).textTheme.bodyText1!.color),
+        ),
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 5,
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: name,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Name",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: password,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () async {
+                if (name.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Name must be non-null"),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                  return;
+                }
+                if (password.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Password must be non-null"),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                  return;
+                }
+                if (await Auth.login(name.text, password.text)) {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => HomePage(4)),
+                      (route) => false);
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => LeaderBoardPage(),
+                    ),
+                  );
+                  return;
+                }
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      "Something went wrong",
+                    ),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              },
+              child: Text(
+                "Continue",
+                style: TextStyle(
+                    color: Theme.of(context).textTheme.bodyText1!.color),
+              ),
+            ),
+          ],
         ),
       ),
     );

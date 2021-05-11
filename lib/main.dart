924623154b08f851d239cffa745af00694d983be
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-05-11 18:29:10 +0100

    Reverted changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:get_storage/get_storage.dart';
 import 'package:improved_2048/ui/homePage.dart';
 import 'package:improved_2048/api/settings.dart';
 
+import 'api/auth.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   await Settings.init();
   await ImportantValues.init();
+  await Auth.init();
 
   runApp(
     MaterialApp(

57fe6a488b6f0c7bfdf17da6ddb95e83fa0b1742
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-05-13 06:43:15 +0100

    Updated LEADERBOARD

--- lib/ui/leaderBoardPage.dart
+++ lib/ui/leaderBoardPage.dart
@@ -6,8 +6,6 @@ import 'package:improved_2048/api/settings.dart';
 import 'package:improved_2048/ui/authenticationPage.dart';
 
 class LeaderBoardPage extends StatefulWidget {
-
-
   static void canSeeLeaderBoard(BuildContext context) {
     if (Auth.loggedIn) {
       Navigator.of(context).push(
@@ -25,7 +23,6 @@ class LeaderBoardPage extends StatefulWidget {
 }
 
 class _LeaderBoardPageState extends State<LeaderBoardPage> {
-
   int whatByWhat = 4;
 
   @override
@@ -48,10 +45,20 @@ class _LeaderBoardPageState extends State<LeaderBoardPage> {
         child: Align(
           alignment: Alignment.center,
           child: StreamBuilder<List<Document>>(
-            stream: Settings.firestore.collection("users").document("scores").collection(whatByWhat.toString()).stream,
+            stream: Settings.firestore
+                .collection("users")
+                .document("scores")
+                .collection(whatByWhat.toString())
+                .stream,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return ListView.builder(itemBuilder: (context, index) => Text("${snapshot.data![index].map["name"]} : ${snapshot.data![index].map["highScore"]}"), itemCount: snapshot.data!.length,);
+                return ListView.builder(
+                  itemBuilder: (context, index) => Center(
+                    child: Text(
+                        "${snapshot.data![index].map["name"]} : ${snapshot.data![index].map["highScore"]}"),
+                  ),
+                  itemCount: snapshot.data!.length,
+                );
               }
               return Text("LOADING ...");
             },

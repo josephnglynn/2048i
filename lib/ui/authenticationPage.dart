9e252ca80f06d1f2820b7ea802865f57c8358f14
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-05-13 17:53:07 +0100

    Updated Widget

--- lib/ui/authenticationPage.dart
+++ lib/ui/authenticationPage.dart
@@ -5,44 +5,21 @@ import 'package:improved_2048/api/auth.dart';
 import 'package:improved_2048/ui/homePage.dart';
 import 'package:improved_2048/ui/leaderBoardPage.dart';
 
-class AuthenticationDialog extends StatefulWidget {
-
+class AuthenticationDialog {
   static Future showAuthDialog(BuildContext context) async {
-    final dialog = AlertDialog(
-      content: AuthenticationDialog(),
-    );
     await showDialog(
       context: context,
-      builder: (context) => dialog,
-    );
-  }
-
-  @override
-  _AuthenticationDialogState createState() => _AuthenticationDialogState();
-}
-
-class _AuthenticationDialogState extends State<AuthenticationDialog> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  "Oop, you have to be logged in to access online services!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextButton(
+      builder: (context) => AlertDialog(
+        content: Text(
+          "Oop, you have to be logged in to access online services!",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextButton(
                 onPressed: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
@@ -57,10 +34,7 @@ class _AuthenticationDialogState extends State<AuthenticationDialog> {
                       fontSize: 25),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextButton(
+              TextButton(
                 onPressed: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
@@ -75,125 +49,13 @@ class _AuthenticationDialogState extends State<AuthenticationDialog> {
                       fontSize: 25),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          )
+        ],
       ),
     );
   }
-}
 
-class Login extends StatefulWidget {
-  @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme:
-            IconThemeData(color: Theme.of(context).textTheme.bodyText1!.color),
-        title: Text(
-          "Login",
-          style: TextStyle(color: Theme.of(context).textTheme.bodyText1!.color),
-        ),
-        elevation: 0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 5,
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: email,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Email",
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: password,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomSheet: Padding(
-        padding: EdgeInsets.all(10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () async {
-                if (email.text.isEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("Email must be non-null"),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                  return;
-                }
-                if (password.text.isEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("Password must be non-null"),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                  return;
-                }
-                if (await Auth.login(email.text, password.text)) {
-                  Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => HomePage(4)),
-                      (route) => false);
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => LeaderBoardPage(),
-                    ),
-                  );
-                  return;
-                }
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      "Something went wrong",
-                    ),
-                    backgroundColor: Colors.red,
-                  ),
-                );
-              },
-              child: Text(
-                "Continue",
-                style: TextStyle(
-                  color: Theme.of(context).textTheme.bodyText1!.color,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
 
 class SignUp extends StatefulWidget {

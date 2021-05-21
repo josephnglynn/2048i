b57397ab5059cb5a500089c1841d20dfdf42140e
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-05-21 14:03:10 +0100

    Fixed Colors

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,60 @@ void main() async {
 
   runApp(
     MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        textTheme: GoogleFonts.openSansTextTheme(),
-      ),
       theme: ThemeData.light().copyWith(
         textTheme: GoogleFonts.openSansTextTheme(),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(
+            backgroundColor: ThemeData.dark().scaffoldBackgroundColor,
+            textTheme: TextTheme(
+                headline6:
+                GoogleFonts.openSans().copyWith(color: Colors.white)),
+            iconTheme: IconThemeData(color: Colors.white)),
+        dialogTheme: DialogTheme(
+            titleTextStyle: TextStyle(color: Colors.white,)
+        ),
+        textTheme: GoogleFonts.openSansTextTheme().copyWith(
+          bodyText2: TextStyle(
+            color: Colors.white,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+          ),
+          caption: TextStyle(
+            color: Colors.white,
+          ),
+          subtitle1: TextStyle(
+            color: Colors.white,
+          ),
+          subtitle2: TextStyle(
+            color: Colors.white,
+          ),
+          headline6: TextStyle(
+            color: Colors.white,
+          ),
+          headline1: TextStyle(
+            color: Colors.white,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+          ),
+          headline3: TextStyle(
+            color: Colors.white,
+          ),
+          headline4: TextStyle(
+            color: Colors.white,
+          ),
+          headline5: TextStyle(
+            color: Colors.white,
+          ),
+
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+              textStyle: TextStyle(foreground: Paint()..color = Colors.white)
+          ),
+        ),
       ),
       themeMode: ThemeMode.system,
       home: HomePage(4),
